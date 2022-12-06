3f1d61e5dc3a0b2ec3672c232270902f2a9a224f
Regina Dulaeva <dulaeva_regina@mail.ru>
2022-12-06 23:37:54 +0300

    first commit

---
+++ Task/Program.cs
@@ -0,0 +1,33 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма.
+
+string[] array;
+Console.WriteLine("Введите через пробел массив строк: ");
+string length = Console.ReadLine();
+array = length.Split(' ');
+string[] newArray = new string[array.Length];
+
+void FindThirdElement(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FindThirdElement(array, newArray);
+PrintArray(newArray);
